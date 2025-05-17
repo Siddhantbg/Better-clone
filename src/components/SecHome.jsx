65d2c2aa97bd379057df9279fee2097a40b90d4f
Siddhant Bhagat <156163973+Siddhantbg@users.noreply.github.com>
2025-05-17 16:33:08 +0530

    hero responsive fix

--- src/components/SecHome.jsx
+++ src/components/SecHome.jsx
@@ -32,20 +32,20 @@ const SecHome = () => {
     <div className="bg-[#fdfcf9] min-h-screen flex flex-col md:flex-row items-center justify-center px-6 py-16 gap-10 md:gap-65">
       <div>
         {/*Image Section */}
-        <div className="max-w-sm w-[341px] relative rounded-2xl overflow-hidden mr-5 shadow-md">
+        <div className="max-w-sm w-[341px] md:top-3 md:right-2 sm:top-70 relative rounded-2xl overflow-hidden mr-5 shadow-md">
           <img
             src={image}
             alt={name}
-            className="w-[341px] h-[606px] object-cover"
+            className="w-[341px] h-[595px] object-cover"
           />
         </div>
-        <div className="flex space-x-4 mt-5 cursor-pointer">
+        <div className="flex relative md:top-2 sm:bottom-100 space-x-4 mt-5 cursor-pointer">
           {Object.keys(testimonials).map((person) => (
             <button
               key={person}
               onClick={() => setSelected(person)}
-              className={`px-6 py-2 cursor-pointer rounded-full transition ${selected === person
-                  ? 'border-2 border-[#2c6b45] text-[#2c6b45] bg-white shadow-md font-extrabold'
+              className={`px-6 py-2 hover:border-3 hover:text-[#2c6b45] hover:border-[#2c6b45] cursor-pointer rounded-full transition ${selected === person
+                  ? 'border-3 border-[#2c6b45] text-[#2c6b45] bg-white shadow-md font-extrabold'
                   : 'border border-gray-300 text-gray-800 bg-white font-medium'
                 }`}
             >
@@ -57,20 +57,19 @@ const SecHome = () => {
 
       </div>
 
-
       {/* 📄 Text Section */}
-      <div className="text-center mt-15 md:text-left max-w-xl">
-        <h2 className="text-4xl sm:text-5xl geist-med font-bold text-[rgb(41,43,41)] leading-[80px] mb-6">
+      <div className="text-center mt-15 md:text-left md:top-5 relative sm:bottom-200 max-w-xl">
+        <h2 className="md:text-7xl sm:text-[47px] geist-imed font-bold text-[rgb(41,43,41)] leading-[80px] mb-6">
           Find out why <br className="hidden md:block" /> we're better
         </h2>
 
 
-        <button className="bg-[#214534] geist-wlight text-white font-medium mt-5 px-12 py-4 rounded-full hover:bg-[#265d3c] transition mb-4">
+        <button className="bg-[#214534] geist-wlight sm:mt-0 text-white sm:px-40 sm:py-4 font-medium md:mt-5 md:px-12 md:py-4 rounded-full hover:bg-[#265d3c] transition mb-4">
           See all our stories
         </button>
 
-        <div className="flex justify-center md:justify-start items-center gap-2 text-sm text-[#1e1e1e] font-medium">
-          <span className="text-green-700 text-3xl absolute md:top-379 right-162">★</span>
+        <div className="flex justify-center md:justify-start items-center gap-2 text-sm text-[#1e1e1e] font-medium ml-5">
+          <span className="text-green-700 text-3xl absolute sm:right-101 sm:top-42 md:top-67 md:right-106">★</span>
           <span className="geist-wlight font-extrabold"><strong>Trustpilot</strong></span>
           <span className="geist-tlight">Excellent 4.4 </span>
           <span className="geist- light">out of 5</span>
